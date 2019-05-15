c267cb0a721fbe9a6134ed611d92d432e6e6324d
Sujeet Kumar <sujeetkv90@gmail.com>
2019-05-15 17:33:02 +0530

    updated cli script

--- flaskcode/cli.py
+++ flaskcode/cli.py
@@ -1,7 +1,7 @@
 import os
 import click
 from flask import Flask
-from . import blueprint
+from . import blueprint, default_config
 
 
 def create_flask_app():
@@ -11,20 +11,19 @@ def create_flask_app():
     return app
 
 
-@click.command()
+@click.command(help='Run %s with given RESOURCE_BASEPATH or current working directory.' % default_config.FLASKCODE_APP_TITLE)
+@click.argument('resource-basepath', default=os.getcwd(), type=os.path.abspath, required=False)
 @click.option('-h', '--host', default='127.0.0.1', help='IP or hostname on which to bind HTTP server')
 @click.option('-p', '--port', default=5001, type=int, help='Port on which to bind HTTP server')
-@click.option('-d', '--resource-basepath', default=None, help='Basepath for resources, default is current working directory')
 @click.option('--debug/--normal', default=False, help='Enter DEBUG mode')
 @click.option('--env', default='development', help='Flask environment, default is development')
-@click.option('--app-title', default=None, help='Application title')
-def run(host, port, resource_basepath, debug, env, app_title):
-    click.echo('FlaskCode CLI')
+def run(resource_basepath, host, port, debug, env):
     os.environ.setdefault('FLASK_ENV', env)
     os.environ.setdefault('FLASK_DEBUG', '1' if debug else '0')
+    click.echo('%s CLI: %s' % (default_config.FLASKCODE_APP_TITLE, resource_basepath))
+    click.echo('')
     app = create_flask_app()
-    app.config['FLASKCODE_RESOURCE_BASEPATH'] = resource_basepath or os.getcwd()
-    app.config['FLASKCODE_APP_TITLE'] = app_title
+    app.config['FLASKCODE_RESOURCE_BASEPATH'] = resource_basepath
     app.run(host=host, port=port, debug=debug)
 
 
